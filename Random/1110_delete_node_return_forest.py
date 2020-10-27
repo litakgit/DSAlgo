91230f332348a4656df2e0e3128a9b4ff4b48453
litakgit <tilakadhya@gmail.com>
2020-10-27 02:11:32 -0700

    Delete nodes to have forests

---
+++ Random/1110_delete_node_return_forest.py
@@ -0,0 +1,48 @@
+import collections
+
+class TreeNode(object):
+    def __init__(self, data=0, left=None, right=None):
+        self.data = data
+        self.left, self.right = left, right
+
+    def __repr__(self):
+        return str(self.data) + " " + str(self.left) + " " + str(self.right)
+
+gpos = 0
+
+def delete_nodes_to_make_forests(root, delete_nodes):
+    def update_forests(nd, pos):
+        if nd and nd.data in delete_nodes and pos not in forest:
+            return
+        forest[pos].append(nd.data if nd and nd.data not in delete_nodes else None)
+
+    def build_forests(node, pos):
+        global gpos
+        update_forests(node, pos)
+        if not node:
+            return
+        if node.data in delete_nodes:
+            if node.left:
+                gpos += 1
+                pos = gpos
+                build_forests(node.left, pos)
+            if node.right:
+                gpos += 1
+                pos = gpos
+                build_forests(node.right, pos)
+        else:
+            build_forests(node.left, pos)
+            build_forests(node.right, pos)
+
+    forest = collections.defaultdict(list)
+    build_forests(root, gpos)
+
+    print (forest.keys())
+    for key in forest:
+        print (forest[key])
+
+if __name__ == "__main__":
+    tree = TreeNode(1, TreeNode(2, TreeNode(4), TreeNode(5, TreeNode(8, TreeNode(9), TreeNode(10)), TreeNode(12))), TreeNode(3, TreeNode(6), TreeNode(7)))
+    print (tree)
+    delete_nodes_to_make_forests(tree, [2, 8, 9, 5])
+    #delete_nodes_to_make_forests(tree, [12])
